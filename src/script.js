d17fc933f97f3907fc15119510bae2d9fd92f87b
bibek190 <bibek9818917036@gmail.com>
2024-11-06 12:51:44 +1100

    create wave

--- src/script.js
+++ src/script.js
@@ -18,30 +18,40 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load("./textures/particles/2.png");
 
 /*
 Particles
 */
-
 const particleGeometry = new THREE.BufferGeometry();
-
-const count = 5000;
+const count = 20000;
 const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
 
 for (let i = 0; i < count * 3; i++) {
-  positions[i] = (Math.random() - 0.5) * 5;
+  positions[i] = (Math.random() - 0.5) * 10;
+  colors[i] = Math.random();
 }
 particleGeometry.setAttribute(
   "position",
   new THREE.BufferAttribute(positions, 3)
 );
-
+particleGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
 const particleMaterial = new THREE.PointsMaterial({
-  size: 0.03,
+  size: 0.1,
   sizeAttenuation: true,
+  alphaMap: particleTexture,
+  color: new THREE.Color("#ff6400"),
+  transparent: true,
+  depthWrite: false,
+  blending: THREE.AdditiveBlending,
+  vertexColors: true,
 });
 const particles = new THREE.Points(particleGeometry, particleMaterial);
+
+// console.log(particleGeometry.attributes.position.array);
 scene.add(particles);
+
 /**
  * Test cube
  */
@@ -78,7 +88,9 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.z = 3;
+camera.position.z = 7;
+camera.position.y = 2;
+
 scene.add(camera);
 
 // Controls
@@ -104,6 +116,21 @@ const tick = () => {
 
   // Update controls
   controls.update();
+
+  // update particles
+  // particles.rotation.y = elapsedTime * 0.2;
+  // particles.rotation.x = elapsedTime * 0.2;
+
+  for (let i = 0; i < count; i++) {
+    const i3 = i * 3;
+    const x = particleGeometry.attributes.position.array[i3 + 0];
+    particleGeometry.attributes.position.array[i3 + 1] = Math.sin(
+      elapsedTime + x
+    );
+  }
+  particleGeometry.attributes.position.needsUpdate = true;
+
+  camera.position.y = Math.sin(elapsedTime);
 
   // Render
   renderer.render(scene, camera);
