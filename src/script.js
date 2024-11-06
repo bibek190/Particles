c70836c7456df801e39245f4e062e74c585e2821
bibek190 <bibek9818917036@gmail.com>
2024-11-06 11:48:46 +1100

    comment

--- src/script.js
+++ src/script.js
@@ -22,26 +22,6 @@ const textureLoader = new THREE.TextureLoader();
 /*
 Particles
 */
-// const particleGeometry = new THREE.BufferGeometry();
-// const count = 500;
-
-// const positions = new Float32Array(count * 3);
-
-// for (let i = 0; i < count * 3; i++) {
-//   positions[i] = (Math.random() - 0.5) * 10;
-// }
-
-// particleGeometry.setAttribute(
-//   "position",
-//   new THREE.BufferAttribute(positions, 3)
-// );
-
-// const particlesMaterial = new THREE.PointsMaterial({
-//   size: 0.02,
-//   sizeAttenuation: true,
-// });
-// const particles = new THREE.Points(particleGeometry, particlesMaterial);
-// scene.add(particles);
 
 const particleGeometry = new THREE.BufferGeometry();
 
